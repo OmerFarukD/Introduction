e07a53eef5451e466ee2950eb933bd5e3dacd5fd
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2025-07-05 22:19:53 +0300

    Arrayler işlendi

---
+++ Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// todo: Kullanıcıdan kaç adet sayı girmek istediğini alınız.
+// girmiş olduğu her sayının toplamını 
+// ortalamasını alan kodu yazınız.
+int[] numbers = GetArray();
+var result = ProcessSumAndAverage(numbers);
+PrintResults(result.sum, result.avg);
+
+
+(double sum , double avg) ProcessSumAndAverage(int[] array)
+{
+    double toplam = 0;
+    foreach (int num in array)
+    {
+        toplam = toplam + num;
+    }
+    double average = toplam / array.Length;
+    return (toplam, average);
+}
+
+int[] GetArray()
+{
+    Console.WriteLine("Lütfen kaç adet sayı almak istediğinizi yazınız.");
+    int count = Convert.ToInt32(Console.ReadLine());
+
+
+    int[] numbers = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        // String interpolation
+        Console.WriteLine($"{i + 1}. Sayı : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        numbers[i] = number;
+    }
+
+    return numbers;
+}
+
+void PrintResults(double toplama , double ortalama)
+{
+    Console.WriteLine($"Dizinin ortalaması : {ortalama}");
+    Console.WriteLine($"Dizinin toplamı : {toplama}");
+}
---
+++ Arrays1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+//object[] arrays = new object[20];
+//ArrayList list = new ArrayList();
+
+
+//list.Add("Buse");
+//list.Add("Ömer");
+//list.Add(false);
+//list.Add(1);
+
+//foreach(var item in list)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+string[] countries = { "Türkiye", "Almanya", "Fransa", "İsviçre" };
+int[] numbers = { 1, 2, 3, 65, 87, 88, 95, 96, 145, 144, 21, 636 };
+//                0  1  2   3   4   5   6   7    8   9    10  11
+//                   +               +       +        +        +
+
+//foreach(string country in countries)
+//{
+//    Console.WriteLine(country);
+//}
+
+
+Array.ForEach(countries, country => Console.WriteLine(country));
+Array.Reverse(countries);
+Console.WriteLine("-------------------------------------------");
+Array.ForEach(countries, country => Console.WriteLine(country));
+Array.Sort(countries);
+Console.WriteLine("-------------------------------------------");
+Array.ForEach(countries, country => Console.WriteLine(country));
+Array.Reverse(countries);
+Console.WriteLine("-------------------------------------------");
+Array.ForEach(countries, country => Console.WriteLine(country));
+
+
+
+// numbers dizisindeki çift sayıları ayrı bir dizide tutan ve bu diziyi ekran çıktısı olarak gösteren kodu yazınız.
+//int count = 0;
+
+//foreach(int num in numbers)
+//{
+//    if(num % 2 == 0)
+//    {
+//        count++;
+//    }
+//}
+
+//int[] filtered = new int[count];
+//int filtered_index = 0;
+//for (int i = 0; i<numbers.Length; i++)
+//{
+//    if (numbers[i] %2 == 0)
+//    {
+//        filtered[filtered_index] = numbers[i];
+//        filtered_index++;
+//    }
+//}
+
+
+//foreach(int filteredNumbers in filtered)
+//{
+//    Console.WriteLine(filteredNumbers);
+//}
+
+// lambda ile gösterimi
+int[] filtered = Array.FindAll(numbers, number => number % 2 == 0);
+Array.ForEach(filtered, filteredNumber => Console.WriteLine(filteredNumber));
+
+
