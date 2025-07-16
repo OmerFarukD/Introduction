32e5b9e3a56d9b7194ff3e36b43df9ee6512a7f1
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2025-07-16 22:13:57 +0300

    OOP giris

---
+++ OOP/Program.cs
@@ -0,0 +1,140 @@
+﻿// Özellik tutan  - Hastane lokasyonu , Hastane Adı 
+// İş yükünü üstlenen - Randevu_Alma , Randevu_İptal Etme
+// SOLID 
+// Single Responsibility 
+
+
+// TODO : Bir kişi Randevu almak istiyor. 
+// Randevu alacak hastanın Adı soyadı yaş bilgileri isteniyorr.
+// Doktor ismi, soyisim ve uzmanlık alanı da isteniyor.
+// Burada randevunun bilgi özetini işlem sonunda kullanıcı istemektedir.
+
+
+var patient = GetPatientInfo();
+var doctor = GetDoctorInfo();
+var appointment = GetAppointmentDateTime();
+string patientFullName = $"{patient.name} {patient.surname}";
+string doctorFullName = doctor.name + " "+ doctor.surname;
+GetAppointmentSummaries(patientFullName,patient.age,appointment, doctorFullName, doctor.speciality);
+
+
+(string name, string surname, int age) GetPatientInfo()
+{
+
+    Console.WriteLine("Lütfen hasta adını giriniz. ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Lütfen hasta Soyadını giriniz. ");
+    string surname = Console.ReadLine();
+
+
+    Console.WriteLine("Lütfen hasta Yaşını giriniz. ");
+    int age = Convert.ToInt16(Console.ReadLine());
+
+
+    return (name, surname, age);
+}
+
+
+(string name, string surname,string speciality) GetDoctorInfo()
+{
+    Console.WriteLine("Lütfen Doktor adını giriniz. ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Lütfen Doktor Soyadını giriniz. ");
+    string surname = Console.ReadLine();
+
+    Console.WriteLine("Lütfen Doktor Uzmanlığını  giriniz. ");
+    string speciality = Console.ReadLine();
+
+    return (name, surname, speciality);
+}
+
+
+
+
+//(int day, string month , int year) GetAppointmentDate()
+//{
+//    Console.WriteLine("Lütfen gün bilgisini yazınız.");
+//    int day = Convert.ToInt32(Console.ReadLine());
+
+
+//    Console.WriteLine("Lütfen yıl bilgisini yazınız.");
+//    int year = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Lütfen ay bilgisini giriniz.");
+//    string month = Console.ReadLine();
+
+//    return (day, month, year);
+
+//}
+
+
+DateTime GetAppointmentDateTime()
+{
+    Console.WriteLine("Lütfen gün bilgisini yazınız.");
+    int day = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("Lütfen ay bilgisini giriniz.");
+    string monthName = Console.ReadLine();
+
+    Console.WriteLine("Lütfen yıl bilgisini yazınız.");
+    int year = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Lütfen Randevu saatini giriniz.");
+    int hour = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Lütfen Randevu dakikasını giriniz.");
+    int minute = Convert.ToInt32(Console.ReadLine());
+
+
+    // Ocak , OCAK , OcAk,OCak
+    int month = monthName.ToLower().Trim() switch
+    {
+        "ocak" => 1,
+        "şubat" => 2,
+        "mart" => 3,
+        "nisan" => 4,
+        "mayıs" => 5,
+        "haziran" => 6,
+        "temmuz" => 7,
+        "ağustos" => 8,
+        "eylül" => 9,
+        "ekim" => 10,
+        "kasım" => 11,
+        "aralık" => 12,
+        _ => 0
+    };
+
+    DateTime appointmentDate = new DateTime(year,month,day,hour,minute,0);
+
+    return appointmentDate;
+}
+
+//void GetAppointmentSummaries(string patientFullName, int patientAge, int day, string month, int year, string doctorFullname, string doctorSpeciality)
+//{
+//    Console.WriteLine("Hasta Bilgileri");
+//    Console.WriteLine("----------------------------------");
+//    Console.WriteLine($"Hasta Ad ve Soyadı : {patientFullName}");
+//    Console.WriteLine($"Hasta yaşı : {patientAge}");
+//    Console.WriteLine($"Doktor Bilgileri");
+//    Console.WriteLine("-----------------------------------");
+//    Console.WriteLine($"Doktor Ad ve Soyad : {doctorFullname}");
+//    Console.WriteLine($"Doktor Uzmanlığı : {doctorSpeciality}");
+//    Console.WriteLine($"Randevu Tarihi : {day} / {month} / {year}");
+//}
+
+
+void GetAppointmentSummaries(string patientFullName, int patientAge, DateTime time, string doctorFullname, string doctorSpeciality)
+{
+    Console.WriteLine("Hasta Bilgileri");
+    Console.WriteLine("----------------------------------");
+    Console.WriteLine($"Hasta Ad ve Soyadı : {patientFullName}");
+    Console.WriteLine($"Hasta yaşı : {patientAge}");
+    Console.WriteLine($"Doktor Bilgileri");
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine($"Doktor Ad ve Soyad : {doctorFullname}");
+    Console.WriteLine($"Doktor Uzmanlığı : {doctorSpeciality}");
+    Console.WriteLine($"Randevu Tarihi : {time.Day} / {time.Month} / {time.Year}  Saat : {time.Hour} : {time.Minute}");
+}
---
+++ OOP_Intro/Program.cs
@@ -0,0 +1,77 @@
+﻿// Özellik tutan  - Hastane lokasyonu , Hastane Adı 
+// İş yükünü üstlenen - Randevu_Alma , Randevu_İptal Etme
+// SOLID 
+// Single Responsibility 
+
+
+// TODO : Bir kişi Randevu almak istiyor. 
+// Randevu alacak hastanın Adı soyadı yaş bilgileri isteniyorr.
+// Doktor ismi, soyisim ve uzmanlık alanı da isteniyor.
+// Burada randevunun bilgi özetini işlem sonunda kullanıcı istemektedir.
+
+
+Patient p = new Patient();
+p.Name = "Ömer";
+p.Surname = "Qubit";
+p.Age = 25;
+
+Patient p1 = new Patient();
+p1.Name = "ASD";
+p1.Surname = "DSA";
+p1.Age = 35;
+
+Patient p3 = new Patient()
+{
+    Age = 25,
+    Name = "Buse",
+    Surname = "Qubit"
+};
+
+Patient p4 = new()
+{
+    Surname = "",
+    Name = "",
+    Age =0
+};
+
+var p5 = new Patient()
+{
+
+    Age = 25,
+    Name = "Buse",
+    Surname = "Qubit"
+};
+
+Console.WriteLine(p3);
+
+string text = p3.ToString();
+Console.WriteLine(text);
+
+
+class Patient 
+{
+
+    // field 
+    public string Name;
+    public string Surname;
+    public int Age;
+
+   //public void EkranaYaz()
+   // {
+   //     Console.WriteLine($"Hasta Adı : {Name}");
+   //     Console.WriteLine($"Hasta Soyadı  : {Surname}");
+   //     Console.WriteLine($"Hasta yaşı : {Age}");
+   // }
+
+    public override string ToString()
+    {
+        return $"Hasta adı : {Name}, Soyadı : {Surname}, Yaş : {Age}";
+    }
+
+
+
+}
+
+
+
+
